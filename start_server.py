1a73df37466696c5ce8586997b1abc45cfef0a04
wilgao09 <william.gao09@gmail.com>
2021-02-07 21:36:19 -0500

    added response headers

--- start_server.py
+++ start_server.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, Response
 
 server = Flask(__name__)
 
@@ -8,17 +8,32 @@ def generic_response(file):
 	# The XSB Terminal library currently assumes all library files are at root.
 	if "xsbInterface" in file or "xsbTerminal" in file:	# If a file from the xsbInterface or xsbTerminal is requested, then send it from ./src/modules/terminal. 
 		if ".wasm" in file: # Manually send file with mimetype according to extension (Flask mimetype inference is bad)
-			return send_from_directory('./src/modules/terminal/', file, mimetype="application/wasm")
+			u = send_from_directory('./src/modules/terminal/', file, mimetype="application/wasm")
+
+			return u
 		elif ".js" in file:
-			return send_from_directory('./src/modules/terminal/', file, mimetype="text/javascript")
-		return send_from_directory('./src/modules/terminal/', file)
+			u = send_from_directory('./src/modules/terminal/', file, mimetype="text/javascript")
+			
+			return u
+		u = send_from_directory('./src/modules/terminal/', file)
 		
-	return send_from_directory("./src/", file)
+		return u
+	
+	u= send_from_directory("./src/", file)
+
+	
+	return u
 
 # Send main webpage when "/" url is queried by client
 @server.route("/")
 def home():
-	return send_from_directory("./src/modules/presentation/", "index.html")
+	u =  send_from_directory("./src/modules/presentation/", "index.html")
+	
+	u.headers.set('Cross-Origin-Opener-Policy','same-origin')
+	u.headers.set('Cross-Origin-Embedder-Policy','require-corp')
+	u.headers.set('X-Content-Type-Options', 'nosniff')
+
+	return u
 
 # Start server on port 8000
 server.run("localhost", "8000")
